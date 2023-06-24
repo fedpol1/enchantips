9f70e233b0a01097b2069d9f44caf85090081596
fedpol1 <fedpolone@gmail.com>
2023-06-23 20:23:30 -0400

    Use non-deprecated option builder methods

--- src/main/java/com/fedpol1/enchantips/config/data/visitor/OptionVisitor.java
+++ src/main/java/com/fedpol1/enchantips/config/data/visitor/OptionVisitor.java
@@ -10,13 +10,13 @@ import java.awt.Color;
 public class OptionVisitor implements DataVisitor {
 
     public Option.Builder<Boolean> visit(BooleanDataEntry.BooleanData data) {
-        return Option.createBuilder(Boolean.class)
+        return Option.<Boolean>createBuilder()
                 .binding(data.getDefaultValue(), data::getValue, data::setValue)
                 .controller(TickBoxControllerBuilder::create);
     }
 
     public Option.Builder<Integer> visit(IntegerDataEntry.IntegerData data) {
-        return Option.createBuilder(Integer.class)
+        return Option.<Integer>createBuilder()
                 .binding(data.getDefaultValue(), data::getValue, data::setValue)
                 .controller(o -> data.getStep() == 0 ?
                         IntegerFieldControllerBuilder.create(o)
@@ -29,7 +29,7 @@ public class OptionVisitor implements DataVisitor {
     }
 
     public Option.Builder<Color> visit(ColorDataEntry.ColorData data) {
-        return Option.createBuilder(Color.class)
+        return Option.<Color>createBuilder()
                 .binding(data.getDefaultValue(), data::getValue, data::setValue)
                 .controller(ColorControllerBuilder::create);
     }
