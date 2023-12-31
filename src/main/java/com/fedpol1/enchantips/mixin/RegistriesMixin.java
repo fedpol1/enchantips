d4ff8b26624d593e375fceff48d201f35547cd87
fedpol1 <fedpolone@gmail.com>
2023-12-30 23:07:06 -0500

    ACTUALLY initialize config after registry freeze

--- src/main/java/com/fedpol1/enchantips/mixin/RegistriesMixin.java
+++ src/main/java/com/fedpol1/enchantips/mixin/RegistriesMixin.java
@@ -2,16 +2,17 @@ package com.fedpol1.enchantips.mixin;
 
 import com.fedpol1.enchantips.config.ModConfig;
 import net.minecraft.Bootstrap;
+import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Bootstrap.class)
-public class BootstrapMixin {
+@Mixin(Registries.class)
+public class RegistriesMixin {
 
     // initialize config AFTER registries are frozen so per-enchant configs are loaded properly
-    @Inject(method = "initialize()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/Registries;bootstrap()V", ordinal = 0, shift = At.Shift.AFTER))
+    @Inject(method = "freezeRegistries()V", at = @At(value = "TAIL"))
     private static void enchantipsConfigInitialize(CallbackInfo ci) {
         ModConfig.registerConfig();
     }
