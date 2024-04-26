abeac6dc6c9680340bb749600c6617e4259c207b
fedpol1 <fedpolone@gmail.com>
2024-04-25 20:49:37 -0400

    Components > NBT

--- src/main/java/com/fedpol1/enchantips/util/EnchantmentListHelper.java
+++ src/main/java/com/fedpol1/enchantips/util/EnchantmentListHelper.java
@@ -1,63 +1,43 @@
 package com.fedpol1.enchantips.util;
 
+import com.fedpol1.enchantips.EnchantipsClient;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.Text;
 
 public abstract class EnchantmentListHelper {
 
-    public static Enchantment getEnchantment(NbtElement e) {
-        EnchantmentLevel ench = EnchantmentLevel.of(e);
-        if(ench == null) { return null; }
-        return ench.getEnchantment();
-    }
-
-    public static int compareEnchantments(NbtElement a, NbtElement b, boolean compareLevels) {
-        int score = 0;
-        int pass = 0;
-
-        // if either a or b are not a compound, stop early
-        if(a instanceof NbtCompound) { score++; pass++; }
-        if(b instanceof NbtCompound) { score--; pass++; }
-        if(pass < 2) { return score; }
-        score = 0; pass = 0;
-
-        NbtCompound compoundA = (NbtCompound)a;
-        NbtCompound compoundB = (NbtCompound)b;
-
-        // if either A or B do not contain a properly formatted enchantment, stop early
-        if(compoundA.contains("id", NbtElement.STRING_TYPE) && compoundA.contains("lvl", NbtElement.NUMBER_TYPE)) { score++; pass++; }
-        if(compoundB.contains("id", NbtElement.STRING_TYPE) && compoundB.contains("lvl", NbtElement.NUMBER_TYPE)) { score--; pass++; }
-        if(pass < 2) { return score; }
-
+    public static int compareEnchantments(Object2IntMap.Entry<RegistryEntry<Enchantment>> a, Object2IntMap.Entry<RegistryEntry<Enchantment>> b, boolean compareLevels) {
         // compare enchantment IDs
-        String enchA = compoundA.getString("id");
-        String enchB = compoundB.getString("id");
-        if(!enchA.contains(":")) { enchA = "minecraft:" + enchA; }
-        if(!enchB.contains(":")) { enchB = "minecraft:" + enchB; }
-        score = enchA.compareTo(enchB);
+        String enchA = a.getKey().getIdAsString();
+        String enchB = b.getKey().getIdAsString();
+        int score = enchA.compareTo(enchB);
         if(score != 0 || !compareLevels) { return score; }
 
         // compare levels
-        return compoundA.getInt("lvl") - compoundA.getInt("lvl");
+        return a.getIntValue() - b.getIntValue();
     }
 
-    public static int countMatches(NbtList a, NbtList b, boolean compareLevels) {
+    public static int countMatches(ItemEnchantmentsComponent a, ItemEnchantmentsComponent b, boolean compareLevels) {
         int matches = 0;
-        for(int j = 0; j < a.size(); j++) {
-            for(int k = 0; k < b.size(); k++) {
-                if(EnchantmentListHelper.compareEnchantments(a.get(j), b.get(k), compareLevels) == 0) {
+        for(Object2IntMap.Entry<RegistryEntry<Enchantment>> aEntry : a.getEnchantmentsMap()) {
+            for(Object2IntMap.Entry<RegistryEntry<Enchantment>> bEntry : b.getEnchantmentsMap()) {
+                if(EnchantmentListHelper.compareEnchantments(aEntry, bEntry, compareLevels) == 0) {
                     matches++;
-                    if(matches == a.size() || matches == b.size()) { return matches; }
+                    if(matches == a.getEnchantmentsMap().size() || matches == b.getEnchantmentsMap().size()) { return matches; }
                 }
             }
         }
         return matches;
     }
 
-    public static boolean sameEnchantments(NbtList a, NbtList b, boolean compareLevels) {
-        if (a.size() != b.size()) { return false; }
-        return countMatches(a, b, compareLevels) == a.size();
+    public static boolean sameEnchantments(ItemEnchantmentsComponent a, ItemEnchantmentsComponent b, boolean compareLevels) {
+        if (a.getEnchantmentsMap().size() != b.getEnchantmentsMap().size()) { return false; }
+        return countMatches(a, b, compareLevels) == a.getEnchantmentsMap().size();
     }
 }
