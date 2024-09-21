191daa67ae37c5af04c7829d5ef23d29ca82e9f9
fedpol1 <fedpolone@gmail.com>
2024-09-21 15:33:56 -0400

    Add list of tags in per-enchantment config

--- src/main/java/com/fedpol1/enchantips/config/tree/visitor/ScreenVisitor.java
+++ src/main/java/com/fedpol1/enchantips/config/tree/visitor/ScreenVisitor.java
@@ -3,9 +3,18 @@ package com.fedpol1.enchantips.config.tree.visitor;
 import com.fedpol1.enchantips.config.ModConfig;
 import com.fedpol1.enchantips.config.tree.*;
 import dev.isxander.yacl3.api.*;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class ScreenVisitor implements TreeVisitor {
 
@@ -45,11 +54,35 @@ public class ScreenVisitor implements TreeVisitor {
     }
 
     public Object visit(EnchantmentGroupNode n, Object data) {
+        OptionDescription.Builder description = OptionDescription.createBuilder()
+                .text(Text.translatable(n.getIdentifier()))
+                .text(Text.literal(""))
+                .text(Text.translatable("enchantips.config.individual_enchantments.option_tooltip.0"));
+        World world = MinecraftClient.getInstance().world;
+
+        boolean tagsKnown = world != null;
+        if(tagsKnown) {
+            Optional<RegistryEntry.Reference<Enchantment>> enchantmentReference = world
+                    .getRegistryManager()
+                    .get(RegistryKeys.ENCHANTMENT)
+                    .getEntry(Identifier.of(n.getIdentifier()));
+            if(enchantmentReference.isPresent()) {
+                for (TagKey<Enchantment> tag : enchantmentReference.get().streamTags().toList()) {
+                    description.text(Text.literal("#").append(Text.literal(tag.id().toString())));
+                }
+            }
+            else { tagsKnown = false; }
+        }
+        if(!tagsKnown) {
+            description.text(Text
+                    .translatable("enchantips.config.individual_enchantments.option_tooltip.1")
+                    .setStyle(Style.EMPTY.withItalic(true))
+            );
+        }
+
         OptionGroup.Builder groupBuilder = OptionGroup.createBuilder()
                 .name(n.getDescription())
-                .description(OptionDescription.createBuilder()
-                        .text(Text.translatable(n.getIdentifier()))
-                        .build())
+                .description(description.build())
                 .collapsed(true);
         for(Map.Entry<String, Node> current : n.getChildren()) {
             groupBuilder = groupBuilder.option((Option<?>) current.getValue().accept(this, groupBuilder));
