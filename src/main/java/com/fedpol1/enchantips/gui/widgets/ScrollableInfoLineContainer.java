798999ee774f3e4c8a190b813b714fca4453c31c
fedpol1 <fedpolone@gmail.com>
2024-11-27 21:10:38 -0500

    Don't compute scroller position every frame

--- src/main/java/com/fedpol1/enchantips/gui/widgets/ScrollableInfoLineContainer.java
+++ src/main/java/com/fedpol1/enchantips/gui/widgets/ScrollableInfoLineContainer.java
@@ -5,15 +5,22 @@ import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 
 public class ScrollableInfoLineContainer extends InfoLineContainer implements Drawable, Element {
 
     //
     private static final Identifier SCROLLER_TEXTURE = Identifier.ofVanilla("widget/scroller");
     private static final Identifier SCROLLER_BACKGROUND_TEXTURE = Identifier.ofVanilla("widget/scroller_background");
+    private static final int SCROLLER_WIDTH = 6;
 
     protected int padding; // not really
     protected int scrollHeight;
+    private int scrollbarX;
+    private int scrollbarY;
+    private int scrollbarHeight;
+    private int scrollerY;
+    private int scrollerHeight;
 
     public ScrollableInfoLineContainer(int padding) {
         super();
@@ -40,24 +47,26 @@ public class ScrollableInfoLineContainer extends InfoLineContainer implements Dr
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
 
-        int scrollbarLength = this.getHeight() + 2 * this.padding;
-        int scrollbarX = this.x + this.width + this.padding - 6;
-        int scrollbarY = this.y - this.padding;
-        int scrollerLength = (int) Math.clamp((float) scrollbarLength * this.getHeight() / super.getHeight(), 32, scrollbarLength);
-        int scrollerY = Math.clamp (
-                (int) (scrollbarY - (float) this.scrollHeight * (scrollbarLength - scrollerLength) / (super.getHeight() - this.getHeight())),
-                scrollbarY,
-                scrollbarY + scrollbarLength - scrollerLength
-        );
-
-        context.drawGuiTexture(RenderLayer::getGuiTextured, SCROLLER_BACKGROUND_TEXTURE, scrollbarX, scrollbarY, 6, scrollbarLength);
-        context.drawGuiTexture(RenderLayer::getGuiTextured, SCROLLER_TEXTURE, scrollbarX, scrollerY, 6, scrollerLength);
+        if(this.scrollerHeight < this.scrollbarHeight) {
+            context.drawGuiTexture(RenderLayer::getGuiTextured, SCROLLER_BACKGROUND_TEXTURE, scrollbarX, scrollbarY, 6, this.scrollbarHeight);
+            context.drawGuiTexture(RenderLayer::getGuiTextured, SCROLLER_TEXTURE, scrollbarX, scrollerY, 6, this.scrollerHeight);
+        }
     }
 
     public void refresh(int index) {
         for(int i = 0; i < this.lines.size(); i++) {
             this.lines.get(i).refresh(i);
         }
+
+        this.scrollbarHeight = this.getHeight() + 2 * this.padding;
+        this.scrollbarX = this.x + this.width + this.padding - SCROLLER_WIDTH;
+        this.scrollbarY = this.y - this.padding;
+        this.scrollerHeight = (int) Math.clamp((float) scrollbarHeight * this.getHeight() / super.getHeight(), 32, scrollbarHeight);
+        this.scrollerY = Math.clamp (
+                (int) (scrollbarY - (float) this.scrollHeight * (scrollbarHeight - this.scrollerHeight) / (super.getHeight() - this.getHeight())),
+                scrollbarY,
+                scrollbarY + scrollbarHeight - this.scrollerHeight
+        );
     }
 
     public void scroll(int s) {
