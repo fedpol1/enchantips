aa794169770a1a09e8f5c3e5b842746dedf53288
fedpol1 <fedpolone@gmail.com>
2025-05-17 20:55:23 -0400

    Add enchantment info to new config screen

--- src/main/java/com/fedpol1/enchantips/config/tree/visitor/ScreenVisitor2.java
+++ src/main/java/com/fedpol1/enchantips/config/tree/visitor/ScreenVisitor2.java
@@ -2,9 +2,17 @@ package com.fedpol1.enchantips.config.tree.visitor;
 
 import com.fedpol1.enchantips.config.tree.*;
 import com.fedpol1.enchantips.gui.widgets.info_line.*;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class ScreenVisitor2 implements TreeVisitor {
 
@@ -37,8 +45,26 @@ public class ScreenVisitor2 implements TreeVisitor {
     }
 
     public Object visit(EnchantmentGroupNode n, Object data) {
-        CollapsibleInfoLine collapsible = new CollapsibleInfoLine(n.getDescription());
+        CollapsibleInfoLine collapsible = new CollapsibleInfoLine(Text.literal(n.getIdentifier()));
         ((InfoMultiLine) data).addLine(collapsible);
+
+        World world = MinecraftClient.getInstance().world;
+        if(world != null) {
+            Optional<RegistryEntry.Reference<Enchantment>> enchantmentReference = world
+                    .getRegistryManager()
+                    .getOrThrow(RegistryKeys.ENCHANTMENT)
+                    .getEntry(Identifier.of(n.getIdentifier()));
+            if(enchantmentReference.isPresent()) {
+                collapsible.addLine(n.getDescription());
+                CollapsibleInfoLine tagLine = new CollapsibleInfoLine(
+                        Text.translatable("enchantips.config.individual_enchantments.option_tooltip.0")
+                );
+                collapsible.addLine(tagLine);
+                for (TagKey<Enchantment> tag : enchantmentReference.get().streamTags().toList()) {
+                    tagLine.addLine(Text.literal("#").append(Text.literal(tag.id().toString())));
+                }
+            }
+        }
 
         for(Map.Entry<String, Node> current : n.getChildren()) {
             current.getValue().accept(this, collapsible);
